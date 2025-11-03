456855b8f480d91381c2d9db8125b755835c3180
Olvine <baloch.arsalan@gmail.com>
2025-11-03 12:11:08 +0300

    Changed testbench with AND gate and added fatal

--- tb/tb_and_gate.v
+++ tb/tb_and_gate.v
@@ -13,17 +13,17 @@ module tb_and_gate;
     initial begin
         $display("Running AND gate test...");
         a = 0; b = 0; #10;
-        if (y !== 0) $display("Test failed for a=0, b=0");
-
+        if (y == 0) $display("Test passed for a=0, b=0");
+		else $fatal(1, "Simulation failed: unexpected value detected!");	
         a = 0; b = 1; #10;
-        if (y !== 0) $display("Test failed for a=0, b=1");
+        if (y == 0) $display("Test passed for a=0, b=1");
         	else $fatal(1, "Simulation failed: unexpected value detected!");
 
         a = 1; b = 0; #10;
-        if (y !== 0) $display("Test failed for a=1, b=0");
+        if (y == 0) $display("Test passed for a=1, b=0");
 		else $fatal(1, "Simulation failed: unexpected value detected!");
         a = 1; b = 1; #10;
-        if (y !== 1) $display("Test failed for a=1, b=1");
+        if (y == 1) $display("Test Passed for a=1, b=1");
 		else $fatal(1, "Simulation failed: unexpected value detected!");
         $display("All tests passed.");
         $finish;
